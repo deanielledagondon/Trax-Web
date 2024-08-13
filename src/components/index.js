888026468c59b426bc423f766fbeb7fd1ff46a88
arjayorong <arjayorong22@gmail.com>
2024-08-13 10:32:13 +0800

    changes in present queue

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { default as AreaCards } from "./dashboard/areaCards/AreaCards";
 export { default as AreaCharts } from "./dashboard/areaCharts/AreaCharts";
 export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
 export { default as AreaTop } from "./dashboard/areaTop/AreaTop";
-export {default as CurrentQueue } from "../screens/Queue/CurrentQueue";
+export {default as CurrentQueue } from "../screens/queue/currentQueue";
 export {default as Timer } from "../components/Queue/timer";
 export {default as Cards} from "../components/analytics/CardsArea";
 export {default as Present} from "../components/Queue/present";
